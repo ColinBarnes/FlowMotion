f8658f8a78bb3ba64cc08986f1df717f9cd7ec8f
Colin <colin.h.t.barnes@gmail.com>
2023-04-18 21:20:47 -0500

    Debugging portrait mode  for mobile

--- src/components/WebcamSettings/WebcamSettings.tsx
+++ src/components/WebcamSettings/WebcamSettings.tsx
@@ -10,11 +10,15 @@ const WebcamSettings = ({ onVideoStream }: Props) => {
     const [camera, setCamera] = useState<MediaStream | null>(null);
 
     const handleEnableMediaAccess = async () => {
+        const allDevices = await navigator.mediaDevices.enumerateDevices();
+        console.log('All Devices', allDevices);
+        console.log('Screen Orientation', screen.orientation.type);
         const deviceList = await (await navigator.mediaDevices.enumerateDevices()).filter(dev => dev.kind === 'videoinput');
         setDevices(deviceList);
         if( onVideoStream ) {
             const camStream = await navigator.mediaDevices.getUserMedia( {
                 video: {
+                    aspectRatio: screen.orientation.type === "portrait-primary" ? 9/16 : 16/9,
                     deviceId: {
                         exact: deviceList[0].deviceId
                     }
