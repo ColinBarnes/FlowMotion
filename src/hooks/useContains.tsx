5830fbe9f9fe8d9985862bc17edd86423e96c964
Colin <colin.h.t.barnes@gmail.com>
2023-04-18 20:45:30 -0500

    Fixing portait mode

--- src/hooks/useContains.tsx
+++ src/hooks/useContains.tsx
@@ -12,22 +12,9 @@ function useContain(width: number, height: number, factor: number = 1): [number,
         if( width === 0 || height === 0) {
             return [1,1,1];
         }
-        const videoAspect = width/height;
-        const screenIsLandscape = screen.aspect > 1;
-        const videoIsLandscape = videoAspect > 1;
-        let scale;
-        if( screenIsLandscape !== videoIsLandscape ) {
-            scale = ( videoIsLandscape ) ? screen.width/width : screen.height/height;
-        } else {
-            if( screenIsLandscape ) {
-                scale = ( screen.aspect < videoAspect ) ? screen.width/width : screen.height/height;
-            } else {
-                scale = ( screen.aspect < videoAspect ) ? screen.height/height : screen.width/width;
-            }
-            
-        }
+        const scale = Math.min( screen.width/width, screen.height/height );
         return [scale * width * factor, scale * height * factor, 1];
-    }, [screen.width, screen.height, screen.aspect, width, height, factor]);
+    }, [screen.width, screen.height, width, height, factor]);
 }
 
 export default useContain;
